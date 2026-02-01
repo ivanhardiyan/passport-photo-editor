84452fc25c61b4e730322d2720e54a70831c354e
ivanhardiyan <93397251+ivanhardiyan@users.noreply.github.com>
2026-02-01 21:25:41 +0700

    Create constants for clothing templates, background colors, and passport photo dimensions

---
+++ utils/constants.py
@@ -0,0 +1,23 @@
+# Constants for Clothing Templates, Background Colors, and Passport Photo Dimensions
+
+# Clothing Templates
+CLOTHING_TEMPLATES = [
+    'Formal',
+    'Casual',
+    'Sport',
+    'Traditional'
+]
+
+# Background Colors
+BACKGROUND_COLORS = [
+    'White',
+    'Light Blue',
+    'Light Gray',
+    'Beige'
+]
+
+# Passport Photo Dimensions (in pixels)
+PASSPORT_PHOTO_DIMENSIONS = {
+    'width': 600,
+    'height': 750
+}
